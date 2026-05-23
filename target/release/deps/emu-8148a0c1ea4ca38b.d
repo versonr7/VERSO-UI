b5aac60683d7093966961c95ce6d49958919f9d4
versonr7 <fastsshadow220@gmail.com>
2026-05-23 12:28:48 +0300

    حذف Cargo.lock، إضافة target/ إلى .gitignore، إصلاح التبعية

--- target/release/deps/emu-8148a0c1ea4ca38b.d
+++ target/release/deps/emu-8148a0c1ea4ca38b.d
@@ -1,8 +1,8 @@
-/data/data/com.termux/files/home/VERSO-UI/target/release/deps/emu-8148a0c1ea4ca38b.d: /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/e15d806/crates/emu/src/lib.rs /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/e15d806/crates/emu/src/interpreter.rs
+/data/data/com.termux/files/home/VERSO-UI/target/release/deps/emu-8148a0c1ea4ca38b.d: /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/811df13/crates/emu/src/lib.rs /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/811df13/crates/emu/src/interpreter.rs
 
-/data/data/com.termux/files/home/VERSO-UI/target/release/deps/libemu-8148a0c1ea4ca38b.rlib: /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/e15d806/crates/emu/src/lib.rs /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/e15d806/crates/emu/src/interpreter.rs
+/data/data/com.termux/files/home/VERSO-UI/target/release/deps/libemu-8148a0c1ea4ca38b.rlib: /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/811df13/crates/emu/src/lib.rs /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/811df13/crates/emu/src/interpreter.rs
 
-/data/data/com.termux/files/home/VERSO-UI/target/release/deps/libemu-8148a0c1ea4ca38b.rmeta: /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/e15d806/crates/emu/src/lib.rs /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/e15d806/crates/emu/src/interpreter.rs
+/data/data/com.termux/files/home/VERSO-UI/target/release/deps/libemu-8148a0c1ea4ca38b.rmeta: /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/811df13/crates/emu/src/lib.rs /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/811df13/crates/emu/src/interpreter.rs
 
-/data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/e15d806/crates/emu/src/lib.rs:
-/data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/e15d806/crates/emu/src/interpreter.rs:
+/data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/811df13/crates/emu/src/lib.rs:
+/data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/811df13/crates/emu/src/interpreter.rs:
--- target/release/deps/thumb_arm-3f4760cc4a1c65e4.d
+++ target/release/deps/thumb_arm-3f4760cc4a1c65e4.d
@@ -1,7 +1,7 @@
-/data/data/com.termux/files/home/VERSO-UI/target/release/deps/thumb_arm-3f4760cc4a1c65e4.d: /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/e15d806/crates/thumb-arm/src/lib.rs
+/data/data/com.termux/files/home/VERSO-UI/target/release/deps/thumb_arm-3f4760cc4a1c65e4.d: /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/a006fe7/crates/thumb-arm/src/lib.rs
 
-/data/data/com.termux/files/home/VERSO-UI/target/release/deps/libthumb_arm-3f4760cc4a1c65e4.rlib: /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/e15d806/crates/thumb-arm/src/lib.rs
+/data/data/com.termux/files/home/VERSO-UI/target/release/deps/libthumb_arm-3f4760cc4a1c65e4.rlib: /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/a006fe7/crates/thumb-arm/src/lib.rs
 
-/data/data/com.termux/files/home/VERSO-UI/target/release/deps/libthumb_arm-3f4760cc4a1c65e4.rmeta: /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/e15d806/crates/thumb-arm/src/lib.rs
+/data/data/com.termux/files/home/VERSO-UI/target/release/deps/libthumb_arm-3f4760cc4a1c65e4.rmeta: /data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/a006fe7/crates/thumb-arm/src/lib.rs
 
-/data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/e15d806/crates/thumb-arm/src/lib.rs:
+/data/data/com.termux/files/home/.cargo/git/checkouts/thumb-arm-9e4f7668c0253790/a006fe7/crates/thumb-arm/src/lib.rs:
